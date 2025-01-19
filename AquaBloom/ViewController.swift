01d03311e4da4be9824d8c68119b4f9f9f70f554
Tanisha Boekstaaf <tanisha.boekstaaf@gmail.com>
2025-01-18 18:47:06 -0600

    Initial Commit

---
+++ AquaBloom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AquaBloom
+//
+//  Created by Tanisha Boekstaaf on 1/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AquaBloomUITests/AquaBloomUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AquaBloomUITestsLaunchTests.swift
+//  AquaBloomUITests
+//
+//  Created by Tanisha Boekstaaf on 1/18/25.
+//
+
+import XCTest
+
+final class AquaBloomUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
